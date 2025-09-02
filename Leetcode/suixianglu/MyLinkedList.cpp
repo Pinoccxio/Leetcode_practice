08d200a90d31c7da6a4d560ad789a71c4cbc12a5
Xin <chenxin_2001@163.com>
2025-09-02 17:13:09 +0800

    feat:finish 24_swapPairs(with/without dummyhead)

--- Leetcode/suixianglu/MyLinkedList.cpp
+++ Leetcode/suixianglu/MyLinkedList.cpp
@@ -122,5 +122,24 @@ ListNode* swapPairs(ListNode* head) {
     head -> next = swapPairs(newHead -> next);
     newHead -> next = head;
     return newHead;
+
+    // ======= Suixianglu Solution =======
+    ListNode* dummyHead = new ListNode(0); // 设置一个虚拟头结点
+    dummyHead->next = head; // 将虚拟头结点指向head，这样方便后面做删除操作
+    ListNode* cur = dummyHead;
+    while(cur->next != nullptr && cur->next->next != nullptr) {
+        ListNode* tmp = cur->next; // 记录临时节点
+        ListNode* tmp1 = cur->next->next->next; // 记录临时节点
+
+        cur->next = cur->next->next;    // 步骤一
+        cur->next->next = tmp;          // 步骤二
+        cur->next->next->next = tmp1;   // 步骤三
+
+        cur = cur->next->next; // cur移动两位，准备下一轮交换
+    }
+    ListNode* result = dummyHead->next;
+    delete dummyHead;
+    return result;
+    // ======= Suixianglu Solution =======
 }
 // <<< LeetCode_24——两两交换链表中的节点 <<<
