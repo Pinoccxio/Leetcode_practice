83ddfb44c78b51cb56d4cc3b5fe49dae90c86703
Xin <chenxin_2001@163.com>
2025-10-04 19:05:57 +0800

    feat: finish orderTraversal of binary_tree in recursive way

--- Leetcode/suixianglu/binaryTree.cpp
+++ Leetcode/suixianglu/binaryTree.cpp
@@ -3,3 +3,53 @@
 //
 
 #include "binaryTree.h"
+
+// >>> LeetCode_144——二叉树的前序遍历 >>>
+// (25/09/29 DONE: 0 ms——100%, 10.75 MB——59.97%)
+void preorder(TreeNode* node, vector<int>& result) {
+    if (node == nullptr) return;
+    result.push_back(node -> val);
+    preorder(node -> left, result);
+    preorder(node -> right, result);
+}
+
+vector<int> binaryTree::preorderTraversal(TreeNode* root) {
+    vector<int> result;
+    preorder(root, result);
+    return result;
+}
+// <<< LeetCode_144——二叉树的前序遍历 <<<
+
+
+// >>> LeetCode_94——二叉树的中序遍历 >>>
+// (25/09/30 DONE: 0 ms——100%, 10.70 MB——67.69%)
+void inorder(TreeNode* node, vector<int>& result) {
+    if (node == nullptr) return;
+    inorder(node -> left, result);
+    result.push_back(node -> val);
+    inorder(node -> right, result);
+}
+
+vector<int> binaryTree::inorderTraversal(TreeNode* root) {
+    vector<int> result;
+    inorder(root, result);
+    return result;
+}
+// <<< LeetCode_94——二叉树的中序遍历 <<<
+
+
+// >>> LeetCode_145——二叉树的后序遍历 >>>
+// (25/09/30 DONE: 0 ms——100%, 10.91 MB——30.94%)
+void postorder(TreeNode* node, vector<int>& result) {
+    if (node == nullptr) return;
+    postorder(node -> left, result);
+    postorder(node -> right, result);
+    result.push_back(node -> val);
+}
+
+vector<int> binaryTree::postorderTraversal(TreeNode* root) {
+    vector<int> result;
+    postorder(root, result);
+    return result;
+}
+// <<< LeetCode_145——二叉树的后序遍历 <<<
--- Leetcode/suixianglu/binaryTree.h
+++ Leetcode/suixianglu/binaryTree.h
@@ -5,8 +5,33 @@
 #ifndef XIN_BINARYTREE_H
 #define XIN_BINARYTREE_H
 
+#pragma once
+
+#include <stack>
+#include <queue>
+
+using namespace std;
 
 class binaryTree {
+public:
+
+    // Definition for a binary tree node.
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+    // LeetCode_144——二叉树的前序遍历
+    vector<int> preorderTraversal(TreeNode* root);
+    // LeetCode_94——二叉树的中序遍历
+    vector<int> inorderTraversal(TreeNode* root);
+    // LeetCode_145——二叉树的后序遍历
+    vector<int> postorderTraversal(TreeNode* root);
+
 };
 
 
